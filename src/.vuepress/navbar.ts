089b1b1c78fec5b6bf767663d1a2e727696508c0
Tsing <ld320321@163.com>
2024-08-13 00:58:03 +0800

    面试宝典

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -8,7 +8,7 @@ export default navbar(
     { text: "数据库技术", icon: "shujuku", link: "/database/mysql/basic/introduce.md",  activeMatch: "^/database/$", },
     { text: "奇谈杂论", icon: "guaishou", link: "/other/server/nginx/introduce.md",  activeMatch: "^/other/$", },
     { text: "面试宝典", icon: "stump", link: "/interview/mybatis.md",  activeMatch: "^/interview/$", },
-    { text: "书籍下载", icon: "jiandurenyuan", link: "/database/mysql/basic/introduce.md",  activeMatch: "^/database/$", },
+    { text: "书籍下载", icon: "jiandurenyuan", link: "/bookdownload/README.md",  activeMatch: "^/bookdownload/$", },
     "/mine/"
   ]
 );
