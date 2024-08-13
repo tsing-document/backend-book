3ebd63c432695dda1b215c9618acab0593ada7ab
Tsing <ld320321@163.com>
2024-08-13 17:30:16 +0800

    公告信息

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -49,7 +49,24 @@ export default hopeTheme({
   // 在这里配置主题提供的插件
   plugins: {
     blog: true,
-
+    // 开启公告信息
+    notice: [
+      {
+        path: "/",
+        title: "Notice Title",
+        confirm: true,
+        fullscreen: true,
+        content: "Notice Content",
+        actions: [
+          {
+            text: "Primary Action",
+            link: "https://theme-hope.vuejs.press/",
+            type: "primary",
+          },
+          { text: "Default Action" },
+        ],
+      },
+    ],
     // 启用之前需安装 @waline/client
     // 警告: 这是一个仅供演示的测试服务，在生产环境中请自行部署并使用自己的服务！
     // comment: {
